b4f969a23af16f81dd949fb61c15fe0aca598e9a
Brandon <bwh1126@gmail.com>
2022-04-06 21:58:26 -0700

    Good to go??

--- g3s.xlarge_TwoInstance/main1.py
+++ g3s.xlarge_TwoInstance/main1.py
@@ -3,6 +3,9 @@ import json
 
 os.environ['TF_CONFIG'] = '{"cluster": {"worker": ["3.136.84.118:12345", "3.143.223.239:23456"]}, "task": {"type": "worker", "index": 0} }'
 
+import numpy as np
+import random
+import pandas as pd
 import time
 import tensorflow as tf
 import keypoint_setup
@@ -19,7 +22,7 @@ num_workers = 2
 strategy = tf.distribute.MultiWorkerMirroredStrategy()
 
 global_batch_size = per_worker_batch_size * num_workers
-multi_worker_dataset = keypoint_setup.mnist_dataset(global_batch_size)
+multi_worker_dataset = keypoint_setup.keypoint_dataset(global_batch_size)
 
 with strategy.scope():
   # Model building/compiling need to be within `strategy.scope()`.
@@ -36,22 +39,71 @@ later = time.time()
 difference = later - now
 print("\nTraining time: {}\n".format(difference))
 
-save_path = "SavedModel/keyPointModel"
-multi_worker_model.save(save_path)
+Test_Dir = '../training.csv'
+test_data = pd.read_csv(Test_Dir)  
 
-#Most recently used instance type
+#dealing with missing values, I've decided to fill null values in instead of dropping them
+#This, unfortunately, will affect accuracy, but I think it's better than dropping them
+test_data.fillna(method = 'ffill',inplace = True)
+
+test_images = []
+for i in range(0,7049):
+    temp_img = test_data['Image'][i].split(' ')
+    temp_img = ['0' if x == '' else x for x in temp_img]
+    test_images.append(temp_img)
+
+
+#converting training and testing images to np.array, and reshaping to be 96 by 96 by 1
+train_images = np.array(test_images,dtype = 'float').reshape(-1,96,96,1)
+
+
+#Now that the images have been set up, the image column can be dropped
+testing = test_data.drop('Image',axis = 1)
+
+test_labels = []
+for i in range(0,7049):
+    temp_label = testing.iloc[i,:]
+    test_labels.append(temp_label)
+
+
+test_labels = np.array(test_labels,dtype = 'float')
+
+#converting training and testing images to np.array, and reshaping to be 96 by 96 by 1
+test_images = np.array(test_images,dtype = 'float').reshape(-1,96,96,1)
+
+
+
+
+# These will be the random indexs of images to print
+# This way I can see how the predictions across several randomly selected images performed
+random_numbers = []
+for i in range(10):
+    random_numbers.append(random.randint(0, len(test_images) - 1))
+
+# Each of these hold 5 sets of locations
+predicted_image_features = []
+predictions = multi_worker_model.predict(test_images)
+
+for number in random_numbers:
+    predicted_image_features.append(predictions[number])
+
+
+plt.figure(figsize=(10, 10))
+for i in range(10):
+    fig1 = plt.subplot(2, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+
+    for j in range(0, 30, 2):
+      fig1.plot(predicted_image_features[i][j],predicted_image_features[i][j+1],'ro', markersize = 3)
+      fig1.plot(test_labels[i][j],test_labels[i][j+1],'bo', markersize = 3)
+      fig1.imshow(test_images[random_numbers[i]].reshape(96, 96), cmap="gray")
+
+
+
 results_dir = "Results/"
 
-plt.plot(multi_worker_model.history.history['mae'])
-plt.title('Training: Mean Absolute Error')
-plt.ylabel('mae')
-plt.xlabel('# epochs')
-plt.savefig(results_dir + 'mae.png', bbox_inches='tight')
-plt.clf()
-plt.plot(multi_worker_model.history.history['accuracy'])
-plt.title('Training: Accuracy')
-plt.ylabel('acc')
-plt.xlabel('# epochs')
-plt.savefig(results_dir + 'acc.png', bbox_inches='tight')
-
-
+plt.subplots_adjust(wspace=10, hspace=0)
+plt.tight_layout()
+plt.savefig(results_dir + 'predictions.png', bbox_inches='tight')
+print('Predictions done')
--- g3s.xlarge_TwoInstance/main2.py
+++ g3s.xlarge_TwoInstance/main2.py
@@ -3,6 +3,9 @@ import json
 
 os.environ['TF_CONFIG'] = '{"cluster": {"worker": ["3.136.84.118:12345", "3.143.223.239:23456"]}, "task": {"type": "worker", "index": 1} }'
 
+import numpy as np
+import random
+import pandas as pd
 import time
 import tensorflow as tf
 import keypoint_setup
@@ -19,7 +22,7 @@ num_workers = 2
 strategy = tf.distribute.MultiWorkerMirroredStrategy()
 
 global_batch_size = per_worker_batch_size * num_workers
-multi_worker_dataset = keypoint_setup.mnist_dataset(global_batch_size)
+multi_worker_dataset = keypoint_setup.keypoint_dataset(global_batch_size)
 
 with strategy.scope():
   # Model building/compiling need to be within `strategy.scope()`.
@@ -36,22 +39,6 @@ later = time.time()
 difference = later - now
 print("\nTraining time: {}\n".format(difference))
 
-save_path = "SavedModel/keyPointModel"
-multi_worker_model.save(save_path)
 
-#Most recently used instance type
-results_dir = "Results/"
-
-plt.plot(multi_worker_model.history.history['mae'])
-plt.title('Training: Mean Absolute Error')
-plt.ylabel('mae')
-plt.xlabel('# epochs')
-plt.savefig(results_dir + 'mae.png', bbox_inches='tight')
-plt.clf()
-plt.plot(multi_worker_model.history.history['accuracy'])
-plt.title('Training: Accuracy')
-plt.ylabel('acc')
-plt.xlabel('# epochs')
-plt.savefig(results_dir + 'acc.png', bbox_inches='tight')
 
 
