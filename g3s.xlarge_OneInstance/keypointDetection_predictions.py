ade512d957d003c38dece3b016df17cc6c3b39c0
Ubuntu <ubuntu@ip-172-31-19-242.us-east-2.compute.internal>
2022-04-07 01:32:55 +0000

    2nd predictions

--- g3s.xlarge_OneInstance/keypointDetection_predictions.py
+++ g3s.xlarge_OneInstance/keypointDetection_predictions.py
@@ -83,6 +83,6 @@ results_dir = "Results/"
 
 plt.subplots_adjust(wspace=10, hspace=0)
 plt.tight_layout()
-plt.savefig(results_dir + 'predictions.png', bbox_inches='tight')
+plt.savefig(results_dir + 'predictions2.png', bbox_inches='tight')
 #plt.show()
 print('Predictions done')
