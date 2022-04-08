6a0cceb3efc3775dbe49e121761e76028f9407bc
Brandon <bwh1126@gmail.com>
2022-04-07 20:58:30 -0700

    removed validation data

--- g3s.xlarge_TwoInstance/main1.py
+++ g3s.xlarge_TwoInstance/main1.py
@@ -50,7 +50,7 @@ difference = later - now
 print("\nInitialization time: {}\n".format(difference))
 now = time.time()
 
-multi_worker_model.fit(multi_worker_dataset, epochs=100, batch_size = global_batch_size, validation_split=0.1)
+multi_worker_model.fit(multi_worker_dataset, epochs=100, batch_size = global_batch_size)
 
 later = time.time()
 difference = later - now
