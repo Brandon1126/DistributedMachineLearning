71968979b36c7e75d68313aeeebd90c494d3296f
Ubuntu <ubuntu@ip-172-31-46-84.us-east-2.compute.internal>
2022-04-20 17:55:27 +0000

    g4dn.xlarge results

--- g4dn.xlarge/run.sh
+++ g4dn.xlarge/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Training Model"
-python3 keypointDetection_training.py | tee Results/g4dn.2xlarge.log
+python3 keypointDetection_training.py |& tee Results/g4dn.xlarge.log
 echo "Done with training"
 python3 keypointDetection_predictions.py
 echo "Done with predictions"
